351fa6ceb97efe446a38282d6ebdbdc6f25465c1
11RIKI11 <159634566+11RIKI11@users.noreply.github.com>
2025-06-05 00:37:29 +0300

    Работают фильтры билетов

--- src/types/ticket/TicketFilterRequest.ts
+++ src/types/ticket/TicketFilterRequest.ts
@@ -5,7 +5,7 @@ export interface TicketFilterRequest {
   eventIds: string[] // массив Guid в виде строк
   buyerIds: string[] // массив Guid в виде строк
   buyerName?: string | null
-  attendeeId: string[] // массив Guid в виде строк
+  attendeeIds: string[] // массив Guid в виде строк
   attendeeName?: string | null
   paymentIds: string[] // массив Guid в виде строк
 }
